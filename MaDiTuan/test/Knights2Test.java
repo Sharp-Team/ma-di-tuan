2f61ffe1972fca938917eb68afed2935b67e25d3
thaycacac <thaycacac@gmail.com>
2019-02-22 23:55:18 +0700

    update bantq

---
+++ MaDiTuan/test/Knights2Test.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Thaycacac
+ */
+public class Knights2Test {
+    
+    public Knights2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class Knights2.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] arg = null;
+        Knights2.main(arg);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of solveProblem method, of class Knights2.
+     */
+    @Test
+    public void testSolveProblem() {
+        System.out.println("solveProblem");
+        int input = 0;
+        int[][] expResult = null;
+        int[][] result = Knights2.solveProblem(input);
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of display method, of class Knights2.
+     */
+    @Test
+    public void testDisplay() {
+        System.out.println("display");
+        int[][] matrix = null;
+        int n = 0;
+        Knights2.display(matrix, n);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ma-di-tuan
+# Ban Master
+Sharp Team
+
+Đằng sau sự Thành Công của người đàn ông luôn có bóng dáng của người phụ nữ.
